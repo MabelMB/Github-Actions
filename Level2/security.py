11f3e794d756db4fcd446271135586b9ba24d85e
reginaarmz <a375925@alumnos.uaslp.mx>
2025-11-23 21:16:46 -0600

    Sabe si esta bien

--- Level2/security.py
+++ Level2/security.py
@@ -1,7 +1,13 @@
 import random
 
 def validar_password(password):
-    if len(password) > 3:
-        return True 
-    return False
+    if len(password) < 8:
+       return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if password.lower() in {"password", "123456", "qwerty", "abc123"}:
+        return False
 
+    return True
